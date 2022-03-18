e80a4d1a102c4395322119855bba0ca70027f788
mboorj <94495260+mboorj@users.noreply.github.com>
2022-03-18 14:05:18 -0700

    Add files via upload

--- htree.cc
+++ htree.cc
@@ -9,41 +9,35 @@
 // Construct a tree
 HTree::HTree(key_t key,
       value_t value,
-      tree_ptr_t left = nullptr,
-      tree_ptr_t right = nullptr);
-  :key_(key), value_(value), left_(left = nullptr), right_(right = nullptr)
+      tree_ptr_t left,
+      tree_ptr_t right)
+  :key_(key), value_(value), left_(left), right_(right)
   {
   }
 
 // recursively destroy a tree
 HTree::~HTree(){
-  if(*this){
-    *this->left_.~HTree();
-    *this->right_.~HTree();
-    delete *this;
-  }
+  this->get_child(HTree::Direction(0)).~HTree();
+  this->get_child(HTree::Direction(1)).~HTree();
+  delete this;
 }
 
 // return the key of the current node
 HTree::key_t HTree::get_key() const{
-  if(*this){
-    return *this->key_;
-  } // what do if not *this?
+  return this->key_;
 }
 
 // return the value of the current node
-HTree::value_t HTree::get_value() const;{
-  if (*this){
-    return *this->value_
-  }
+HTree::value_t HTree::get_value() const{
+  return this->value_;
 }
 
 // get the child indicated by the directions
 HTree::tree_ptr_t HTree::get_child(Direction dir) const{
-  if (dir == 0){
-    return *this->left_;
-  } else if (dir == 1){
-    return *this->right_
+  if (dir == HTree::Direction(0)){
+    return this->left_;
+  } else if (dir == HTree::Direction(1)){
+    return this->right_;
   }
 }
 
@@ -52,13 +46,13 @@ HTree::tree_ptr_t HTree::get_child(Direction dir) const{
 HTree::possible_path_t
 HTree::path_to(key_t key) const
 {
-  if (*this == nullptr){ //??? how to check if not *this
-    return nullptr;
-  }
+  //if (!this){ //??? how to check if not *this
+    //return nullptr;
+  //}
 
   HTree::path_t path;
-  auto pointPath(std::make_unique<HTree::possible_path_t>(path)); // will this point to the right thing if path grows?
   if (this.get_key() == key){
+    auto pointPath(std::make_unique<HTree::possible_path_t>(path));
     return pointPath;
   }
 
@@ -66,6 +60,7 @@ HTree::path_to(key_t key) const
   if (!*testLeft.empty()){
     path.push_back(HTree::Direction(0));
     path.push_back(*testLeft);
+    auto pointPath(std::make_unique<HTree::possible_path_t>(path));
     return pointPath;
   }
 
@@ -75,7 +70,8 @@ HTree::path_to(key_t key) const
   } else {
     path.push_back(HTree::Direction(1));
     path.push_back(*testRight);
-    return *pointPath
+    auto pointPath(std::make_unique<HTree::possible_path_t>(path)); 
+    return *pointPath;
   }
 
 }
