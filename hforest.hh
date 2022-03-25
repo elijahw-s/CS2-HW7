a914c60d985b2848649c3a7c9ff2d39774afb334
mboorj <94495260+mboorj@users.noreply.github.com>
2022-03-25 16:32:43 -0700

    Add files via upload
    specific errors in hforest.cc are included as comments on the relevant line. I've bee using https://en.cppreference.com/w/cpp/algorithm/make_heap as a basis for the syntax and techniques of hforest

---
+++ hforest.hh
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "htree.cc"
+
+class HForest {
+  public:
+
+    using tree_ptr_t = std::shared_ptr<const HTree>;
+    using forest_ptr_t = std::shared_ptr<HForest>;
+    using vector_t = std::vector<tree_ptr_t>;
+    using vector_ptr_t = std::shared_ptr<vector_t>;
+
+    // construct a forest
+    HForest(vector_ptr_t trees);
+
+    // returns number of trees in forest
+    int size() const;
+
+    // add a tree to the forest
+    void add_tree(tree_ptr_t tree);
+
+    // remove lowest-root tree, return pointer
+    tree_ptr_t pop_tree();
+
+
+  private:
+    bool compare_trees(tree_ptr_t t1, tree_ptr_t t2);
+    vector_ptr_t vec_;
+};
