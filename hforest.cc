bbba77716407a8980b23f2e7ab4d60f480d88a7d
mboorj <94495260+mboorj@users.noreply.github.com>
2022-03-27 20:05:47 -0700

    Add files via upload

--- hforest.cc
+++ hforest.cc
@@ -4,8 +4,8 @@
 #include <vector>
 
 HForest::HForest(vector_ptr_t trees){
-  std::make_heap(*trees->begin(), *trees->end(), HForest::compare_trees);
   this->vec_ = trees;
+  std::make_heap(*trees->begin(), *trees->end(), HForest::compare_trees);
 }
 
 void HForest::add_tree(tree_ptr_t tree){
@@ -13,9 +13,10 @@ void HForest::add_tree(tree_ptr_t tree){
   std::push_heap(this->vec_->begin(), this->vec_->end(), HForest::compare_trees);
 }
 
-HForest::tree_ptr_t 
+HForest::tree_ptr_t
 HForest::pop_tree(){
-  this->vec_->pop_heap(this->vec_->begin(), this->vec_->end(), HForest::compare_trees); // error: no member named 'pop_heap' in 'std::vector<std::shared_ptr<const HTree>>'
+  std::pop_heap(this->vec_->begin(), this->vec_->end(), HForest::compare_trees);
+  //this->vec_->pop_heap(this->vec_->begin(), this->vec_->end(), HForest::compare_trees); // error: no member named 'pop_heap' in 'std::vector<std::shared_ptr<const HTree>>'
   tree_ptr_t smallest = this->vec_->back(); // I think this is where the later problems are coming from??
   this->vec_->pop_back();
   return smallest;  // error: no viable conversion from returned value of type 'HForest::tree_ptr_t' (aka 'shared_ptr<const HTree>') to function return type 'int'
--- test_hforest.cc
+++ test_hforest.cc
@@ -1,3 +1,66 @@
+#include "hforest.hh"
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+HTree::tree_ptr_t create_test_tree_1()
+{
+  return
+    std::make_shared<HTree>(126, 1, std::make_shared<HTree>(-5, 2,
+      std::make_shared<HTree>(12, 3),
+      std::make_shared<HTree>(6, 4,
+        std::make_shared<HTree>(9, 5))),
+    std::make_shared<HTree>(12, 6,
+      std::make_shared<HTree>(3, 7)));
+}
+
+HTree::tree_ptr_t create_test_tree_2()
+{
+  return
+    std::make_shared<HTree>(2,100, std::make_shared<HTree>(3,6),
+      std::make_shared<HTree>(4,12));
+}
+
+HTree::tree_ptr_t create_test_tree_3()
+{
+  return
+    std::make_shared<HTree>(4,-21, std::make_shared<HTree>(6,0,
+      std::make_shared<HTree>(5,10), std::make_shared<HTree>(32,-45)),
+      std::make_shared<HTree>(4,12,
+      nullptr, std::make_shared<HTree>(33,12)));
+}
+
+void test_hforest(){
+  auto tree1 = create_test_tree_1(); // root value == 1
+  auto tree2 = create_test_tree_2(); // root value == 100
+  auto tree3 = create_test_tree_3(); // root value == -21
+
+  //std::vector<HTree::tree_ptr_t> treeVec = {tree1};
+  std::shared_ptr<HForest::vector_t> treesVec(new HForest::vector_t({tree1}));
+
+  auto forest = std::make_shared<HForest>(treesVec);
+  assert(forest->size() == 1);
+
+  forest->add_tree(tree2);
+  assert(forest->size() == 2);
+
+  forest->add_tree(tree3);
+  assert(forest->size() == 3);
+
+  auto lowest = forest->pop_tree();
+  assert(lowest->get_value() == tree3->get_value());
+  assert(forest->size() == 2);
+
+  auto middle = forest->pop_tree();
+  assert(middle->get_value() == tree1->get_value());
+  assert(forest->size() == 1);
+
+  auto highest = forest->pop_tree();
+  assert(highest->get_value() == tree2->get_value());
+  assert(forest->size() == 0);
+}
+
 int main(){
+  test_hforest();
   return 0;
 }
