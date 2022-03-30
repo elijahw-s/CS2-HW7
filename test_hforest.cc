219c737b99291994f66a2a038b801f4752c865df
mboorj <94495260+mboorj@users.noreply.github.com>
2022-03-29 21:36:53 -0700

    Add files via upload

--- test_hforest.cc
+++ test_hforest.cc
@@ -51,22 +51,24 @@ void test_hforest(){
   auto tree5 = create_test_tree_5(); // root value == 1
 
 
-  auto trees = std::vector<HForest::tree_ptr_t>{tree1}; // not initalizing well, no matching constructor for initialization of...
-  //HForest::tree_vector_t trees = HForest::tree_vector_t{tree1};
+  auto trees = std::vector<HForest::tree_ptr_t>{tree1};
+
 
   auto forest = std::make_shared<HForest>(trees); // contains roots: 17
   assert(forest->size() == 1);
+  std::cout << "Initalized forest with 1 tree.\n";
 
   forest->add_tree(tree2); // contains roots: 17, 100
   assert(forest->size() == 2);
 
   forest->add_tree(tree3); // contains roots: 17, 100, 1
   assert(forest->size() == 3);
-
+  std::cout << "Added two more trees.\n";
 
   auto lowest = forest->pop_tree(); // contains roots: 17, 100
   assert(lowest->get_value() == tree3->get_value());
   assert(forest->size() == 2);
+  std::cout << "Removed lowest-value tree. Key was: " << lowest->get_value() << '\n';
 
   forest->add_tree(tree4); // contains roots: 17, 100, 33
   assert(forest->size() == 3);
@@ -76,29 +78,58 @@ void test_hforest(){
 
   forest->add_tree(tree3); // contains roots: 17, 100, 33, 1, 1
   assert(forest->size() == 5);
+  std::cout << "Added three more trees.\n";
 
   auto newLowest = forest->pop_tree(); // contains roots: 17, 100, 33, 1
   assert(newLowest->get_value() == tree3->get_value()); // could also be tree5->get_value()
   assert(forest->size() == 4);
+  std::cout << "Removed lowest-value tree. Key was: " << newLowest->get_value() << '\n';
 
   auto secLowest = forest->pop_tree(); // contains roots: 17, 100, 33
   assert(secLowest->get_value() == tree5->get_value()); // could also be tree3->get_value()
   assert(forest->size() == 3);
+  std::cout << "Removed lowest-value tree. Key was: " << secLowest->get_value() << '\n';
 
   auto middle = forest->pop_tree(); // contains roots: 100, 33
   assert(middle->get_value() == tree1->get_value());
   assert(forest->size() == 2);
+  std::cout << "Removed lowest-value tree. Key was: " << middle->get_value() << '\n';
 
   auto secHighest = forest->pop_tree(); // contains roots: 100
   assert(secHighest->get_value() == tree4->get_value());
   assert(forest->size() == 1);
+  std::cout << "Removed lowest-value tree. Key was: " << secHighest->get_value() << '\n';
 
   auto highest = forest->pop_tree(); // empty
   assert(highest->get_value() == tree2->get_value());
   assert(forest->size() == 0);
+  std::cout << "Removed lowest-value tree. Key was: " << highest->get_value() << '\n';
+  std::cout << "Forest is empty.\n\n";
+
+  auto forest2 = std::make_shared<HForest>();
+  assert(forest2->size() == 0);
+  std::cout << "Initalized empty forest.\n";
+
+  forest2->add_tree(tree3);
+  forest2->add_tree(tree5);
+  assert(forest2->size() == 2);
+  std::cout << "Added two trees to second forest.\n";
+
+  auto lowest2 = forest2->pop_tree();
+  assert(lowest2->get_value() == 1);
+  assert(forest2->size() == 1);
+  std::cout << "Removed lowest-value tree. Key was: " << lowest2->get_value() << '\n';
+
+  auto highest2 = forest2->pop_tree();
+  assert(highest2->get_value() == 1);
+  assert(forest2->size() == 0);
+  std::cout << "Removed lowest-value tree. Key was: " << highest2->get_value() << '\n';
+  std::cout << "Forest is empty.\n";
+
 }
 
 int main(){
   test_hforest();
+  std::cout << "\n\n\n";
   return 0;
 }
